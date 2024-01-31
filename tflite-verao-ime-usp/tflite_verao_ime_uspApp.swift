f0d3f437f960f4dcf0e90e0db182d132a6a656ae
Ezequiel dos Santos <ezequiel.santos@miniclip.com>
2024-01-31 00:35:50 +0000

    Initial Commit

---
+++ tflite-verao-ime-usp/tflite_verao_ime_uspApp.swift
@@ -0,0 +1,17 @@
+//
+//  tflite_verao_ime_uspApp.swift
+//  tflite-verao-ime-usp
+//
+//  Created by Ezequiel dos Santos on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct tflite_verao_ime_uspApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
